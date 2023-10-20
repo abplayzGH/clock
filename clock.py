ad2d2127404be81ea7f500ff9e0de387cf59cd7d
Adam Lawrence <adam@impostertwo.com>
2023-10-20 12:55:05 -0500

    dsaq

--- clock.py
+++ clock.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from turtle import *
-#background
+# background
 # color("white")
 # penup()
 # goto(-200,-200)
@@ -9,62 +9,48 @@ from turtle import *
 # begin_fill()
 # circle(400,360,4)
 # end_fill()
-
-two = Turtle()
-
-hour = datetime.now().hour
-minute = datetime.now().minute
-second = datetime.now().second
-hideturtle()
-speed(0)
-pensize(2)
-penup()
-goto(0,-200)
-pendown()
-circle(200)
-penup()
-goto(0,0)
-pendown()
+# tracer(0,0)
+def main():
+    hideturtle()
+    speed(0)
+    pensize(2)
+    penup()
+    goto(0,-200)
+    pendown()
+    circle(200)
+    penup()
+    goto(0,0)
+    pendown()
 
 def second_hand(second):
+    tracer(0,0)
     color("black")
     right(second*6)
     forward(175)
     backward(175)
     left(second*6)
+    update()
 
-def erase_sec(second):
-    second -= 1 
-    right(second*6)
-    color("white")
-    forward(175)
-    backward(175)
-    left(second*6)
-
-def erase_min(minute):
-    minute -= 1 
-    right(minute*6)
-    color("white")
-    forward(150)
-    backward(150)
-    left(minute*6)
 
 
 def minute_hand(minute):
+    tracer(0,0)
     color("red")
     right(minute*6)
     forward(150)
     backward(150)
     left(minute*6)
+    update()
 #forever
 while(True):
+    second 
     hour = datetime.now().hour
     minute = datetime.now().minute
     second = datetime.now().second
-    if second == 59:
-        minute_hand(minute)
-        erase_min(minute) 
-    second_hand(second)
-    erase_sec(second)
-
-
+    main()
+    minute_hand(minute - 15)
+    if  second ==
+    second_hand(second - 15)
+    print(datetime.now().second)
+    clear()
+    
